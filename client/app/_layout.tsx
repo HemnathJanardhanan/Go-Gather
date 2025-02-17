2faf3cd76d24ff318e9bdef0fb763cf64aef486c
celshee <sheenaxelin@.com>
2025-02-18 00:15:12 +0530

    added fonts

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,5 +1,27 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from 'expo-splash-screen';
+
 import "./globals.css";
+import {useFonts} from "expo-font";
+import { useEffect } from "react";
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    "Nunito-Bold": require("../assets/fonts/Nunito-Bold.ttf"),
+    "Nunito-Medium": require("../assets/fonts/Nunito-Medium.ttf"),
+    "Nunito-Regular": require("../assets/fonts/Nunito-Regular.ttf"),
+    "Nunito-ExtraBold": require("../assets/fonts/Nunito-ExtraBold.ttf"),
+    "Nunito-ExtraLight": require("../assets/fonts/Nunito-ExtraLight.ttf"),
+    "Nunito-SemiBold": require("../assets/fonts/Nunito-SemiBold.ttf"),
+    "Nunito-Light": require("../assets/fonts/Nunito-Light.ttf"),
+
+  });
+  useEffect(()=>{
+    if(fontsLoaded){
+      SplashScreen.hideAsync();
+    }
+  },[fontsLoaded]);
+    if(!fontsLoaded){
+      return null;
+    }
+  return <Stack screenOptions={{headerShown:false}}/>
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}","./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        nunito:['Nunito-Regular','sans-serif'],
+        "nunito-medium":['Nunito-Medium','sans-serif'],
+        "nunito-bold":['Nunito-Bold','sans-serif'],
+        "nunito-ExtraBold":['Nunito-ExtraBold','sans-serif'],
+        "nunito-SemiBold":['Nunito-SemiBold','sans-serif'],
+        "nunito-Light":['Nunito-Light','sans-serif'],
+        "nunito-ExtraLight":['Nunito-ExtraLight','sans-serif'],
+      },
+      colors:{
+        primary: {
+          100: '#DFF2EB', // Lightest shade
+          200: '#B9E5E8', // Light
+          300: '#7AB2D3', // Primary color
+        },
+        accent: {
+          100: '#A1B5C6', // Lightest accent shade
+          200: '#6F8DAB', // Light accent
+          300: '#4A628A', // Accent color
+        },
+        black:{
+          DEFAULT:'#000000',
+          100:'#8C8E98',
+          200:'#666876',
+          300:'#191d31',
+        },
+        danger:'#F75555'
+      }
+    },
   },
   plugins: [],
 }
